6cf85a8884545781b648655bd22716de8affd531
phhui <phhui@qq.com>
2013-09-24 16:25:14 +0800

    小菜flash资源打包工具1.0
    打包指定目录及其子目录下所有XML资源和图片资源并编译成SWF
    需要FLEX SDK及JAVA编译环境

---
+++ ResourcesPackaging/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace xmlPackaging
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt_xmlurl = new System.Windows.Forms.TextBox();
+            this.btn_packing = new System.Windows.Forms.Button();
+            this.txt_fileName = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.txt_output = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_xmlurl
+            // 
+            this.txt_xmlurl.Location = new System.Drawing.Point(8, 7);
+            this.txt_xmlurl.Name = "txt_xmlurl";
+            this.txt_xmlurl.ReadOnly = true;
+            this.txt_xmlurl.Size = new System.Drawing.Size(267, 21);
+            this.txt_xmlurl.TabIndex = 0;
+            this.txt_xmlurl.Text = "点击选择资源目录";
+            this.txt_xmlurl.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txt_xmlurl_MouseClick);
+            // 
+            // btn_packing
+            // 
+            this.btn_packing.Location = new System.Drawing.Point(171, 33);
+            this.btn_packing.Name = "btn_packing";
+            this.btn_packing.Size = new System.Drawing.Size(104, 23);
+            this.btn_packing.TabIndex = 2;
+            this.btn_packing.Text = "开 始 打 包";
+            this.btn_packing.UseVisualStyleBackColor = true;
+            this.btn_packing.Click += new System.EventHandler(this.btn_packing_Click);
+            // 
+            // txt_fileName
+            // 
+            this.txt_fileName.Location = new System.Drawing.Point(10, 33);
+            this.txt_fileName.MaxLength = 12;
+            this.txt_fileName.Name = "txt_fileName";
+            this.txt_fileName.Size = new System.Drawing.Size(155, 21);
+            this.txt_fileName.TabIndex = 4;
+            this.txt_fileName.Text = "res.swf";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(26, 114);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(24, 20);
+            this.webBrowser1.TabIndex = 5;
+            this.webBrowser1.Url = new System.Uri("http://www.56lea.com", System.UriKind.Absolute);
+            // 
+            // txt_output
+            // 
+            this.txt_output.Location = new System.Drawing.Point(6, 60);
+            this.txt_output.Multiline = true;
+            this.txt_output.Name = "txt_output";
+            this.txt_output.ReadOnly = true;
+            this.txt_output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_output.Size = new System.Drawing.Size(272, 98);
+            this.txt_output.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 161);
+            this.Controls.Add(this.txt_output);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.txt_fileName);
+            this.Controls.Add(this.btn_packing);
+            this.Controls.Add(this.txt_xmlurl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "小菜flash资源打包工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_xmlurl;
+        private System.Windows.Forms.Button btn_packing;
+        private System.Windows.Forms.TextBox txt_fileName;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox txt_output;
+    }
+}
+
---
+++ ResourcesPackaging/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace xmlPackaging
+{
+    public partial class Form1 : Form
+    {
+        private string xmlurl;
+        private string fname;
+        private string key="C:/Windows/df.dll";
+        private List<FileInfo> fileListData = new List<FileInfo>(); //保存所有的文件信息 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txt_xmlurl_MouseClick(object sender, MouseEventArgs e)
+        {
+            FolderBrowserDialog fb = new FolderBrowserDialog();
+            if (fb.ShowDialog() == DialogResult.OK)
+            {
+                txt_xmlurl.Text = fb.SelectedPath;
+            }
+        }
+
+        private void btn_packing_Click(object sender, EventArgs e)
+        {
+            txt_output.Text = "";
+            if (txt_xmlurl.Text.Length < 5 || txt_xmlurl.Text == "点击选择XML目录")
+            {
+                MessageBox.Show("请选择XML目录");
+                return;
+            }
+            if(txt_fileName.Text.Length<1){
+                MessageBox.Show("文件名不能为空");
+                return;
+            }
+            int n = Convert.ToInt32(getNum());
+            if (n < 1)
+            {
+                MessageBox.Show("SORRY,该软件免费使用已到期，如需要请联系QQ41203160购买！");
+                return;
+            }
+            else
+            {
+                n = n - 1;
+                setNum(n);
+            }
+            if (fname!=null) File.Delete(xmlurl+"/"+fname);
+            fname = txt_fileName.Text;
+            xmlurl = txt_xmlurl.Text;
+            File.WriteAllText("xml.data", xmlurl);
+            txt_output.Text += "正在读取文件列表。。。\r\n";
+            getFile(xmlurl, true);
+            txt_output.Text += "文件列表读取完毕。\r\n";
+            packing();
+            exec();
+        }
+        private void getFile(string url,Boolean contains)
+        {
+            if (!Directory.Exists(url))
+            {
+                MessageBox.Show("XML路径无效~！");
+                return;
+            }           
+            DirectoryInfo directory = new DirectoryInfo(url);
+            DirectoryInfo[] directoryArray = directory.GetDirectories();
+            FileInfo[] fileInfoArray = directory.GetFiles();
+            if (fileInfoArray.Length > 0) fileListData.AddRange(fileInfoArray);
+            if (contains)
+            {
+                foreach (DirectoryInfo _directoryInfo in directoryArray)
+                {//遍历子目录
+                    getFile(_directoryInfo.FullName, contains);
+                }
+            }
+        }
+        private void packing()
+        {
+            txt_output.Text += "正在创建打包程序。。。\r\n";
+            string str = "package  {	import flash.display.Sprite;	public class fl extends Sprite{";            
+            foreach (FileInfo fi in fileListData)
+            {
+                if (checkFile(fi.Name) == 1)
+                {
+                    str += "[Embed(source=\"" + fi.Name + "\", mimeType=\"application/octet-stream\")]		public var " + fi.Name.Substring(0, fi.Name.IndexOf(".")) + ":Class;";
+                }
+                else if (checkFile(fi.Name) == 2) 
+                {
+                    str += "[Embed(source=\"" + fi.Name + "\")]		public var " + fi.Name.Substring(0, fi.Name.IndexOf(".")) + ":Class;";
+                }
+            }
+            str += "		public function fl() {					}	}	}";
+            File.WriteAllText(xmlurl+"/fl.as", str);
+            txt_output.Text += "打包程序创建完毕，准备开始打包。。。\r\n";
+        }
+        private int checkFile(string f)
+        {
+            if (f.Length < 5) return 0;
+            if (f.IndexOf(".") == -1) return 0;
+            f = f.ToLower();
+            f = f.Substring(f.Length - 5);
+            if (f.IndexOf(".") == -1) return 0;
+            if (f.IndexOf(".jpeg") == -1) return 0;
+            else return 2;
+            f = f.Substring(1);
+            if (f==".xml") return 1;
+            if (f==".jpg") return 2;
+            if (f==".gif") return 2;
+            if (f==".png") return 2;
+            if (f==".bmp") return 2;
+            return 0;
+        }
+        private void exec() {
+            File.WriteAllText(xmlurl + "/p.bat","cd "+xmlurl + "\r\n mxmlc fl.as");
+            txt_output.Text += "开始打包，请稍候。。。\r\n";
+            string res = openFile(xmlurl + "/p.bat");
+            txt_output.Text += "打包完毕，结果：\r\n" + res;
+            release();
+            if (res.IndexOf("fl.swf") != -1)
+            {
+                MessageBox.Show("打包完毕，资源已成功打包！");
+            }
+            else
+            {
+                MessageBox.Show("错误:资源未能成功打包，请检查输出结果！");
+            }
+        }
+        private void release()
+        {
+            if (File.Exists(xmlurl + "/fl.as")) File.Delete(xmlurl + "/fl.as");
+            if (File.Exists(xmlurl + "/p.bat")) File.Delete(xmlurl + "/p.bat");
+            if (File.Exists(xmlurl + "/" + fname)) File.Delete(xmlurl + "/" + fname);
+            File.Copy(xmlurl + "/fl.swf", xmlurl + "/" + fname);
+            File.Delete(xmlurl + "/fl.swf");
+        }
+        private string openFile(string fileName)
+        {
+            string res="";
+            Process p = new Process();
+            int milliseconds = 30000;
+            p.StartInfo.FileName = fileName;
+
+            p.StartInfo.UseShellExecute = false;
+
+            p.StartInfo.RedirectStandardInput = true;
+
+            p.StartInfo.RedirectStandardOutput = true;
+
+            p.StartInfo.RedirectStandardError = true;
+
+            p.StartInfo.CreateNoWindow = true;
+            try
+            {
+                if (p.Start())       //开始进程
+                {
+                    if (milliseconds == 0)
+                        p.WaitForExit();     //这里无限等待进程结束
+                    else
+                        p.WaitForExit(milliseconds);  //这里等待进程结束，等待时间为指定的毫秒
+                    res = p.StandardOutput.ReadToEnd();//读取进程的输出
+                }
+            }
+            catch
+            {
+            }
+            finally
+            {
+                if (p != null)
+                    p.Close();
+            }
+            return res;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("xml.data"))
+            {
+                txt_xmlurl.Text = File.ReadAllText("xml.data");
+            }
+            if (!File.Exists(key))
+            {
+                setNum(250);
+            }
+        }
+        private void setNum(int n)
+        {
+            File.WriteAllText(key,DataSafe.DESEncrypt(n.ToString(),"56leacom"));
+        }
+        private string getNum(){
+            string str="0";
+            if (File.Exists(key))
+            {
+                str = DataSafe.DESDecrypt(File.ReadAllText(key),"56leacom");
+            }
+            return str;
+        }
+    }
+}
